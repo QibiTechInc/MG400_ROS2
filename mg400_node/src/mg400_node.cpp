ee171f241e17e8fdc9f180081d04b75ea8573271
Tomoaki Osada <42052727+ynyBonfennil@users.noreply.github.com>
2025-03-13 19:56:08 +0900

    Stop reconnection when `auto_connect` is false

--- mg400_node/src/mg400_node.cpp
+++ mg400_node/src/mg400_node.cpp
@@ -135,7 +135,7 @@ CallbackReturn MG400Node::on_deactivate(const State &)
   this->cancelTimer();
   this->interface_->deactivate();
 
-  if (this->connection_interrupted_) {
+  if (this->get_parameter("auto_connect").as_bool() || this->connection_interrupted_) {
     RCLCPP_INFO(this->get_logger(), "Try reconnecting in 5 seconds ...");
     this->connect_timer_ = this->create_wall_timer(5s, [this]() {this->activate();});
   }
