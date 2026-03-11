8d79eb0a4c35ac6aa93860483bf442936d2ad16b
Yuki Yamamoto <yuki8mamo10.hu@gmail.com>
2026-03-11 19:19:18 +0900

    Refactor mg400_common around canonical kinematics

--- mg400_common/include/mg400_common/mg400_ik_util.hpp
+++ mg400_common/include/mg400_common/mg400_ik_util.hpp
@@ -15,52 +15,49 @@
 #ifndef __MG400_COMMON_MG400_IK_UTIL_HPP__
 #define __MG400_COMMON_MG400_IK_UTIL_HPP__
 
+#include <cmath>
 #include <vector>
 #include <eigen3/Eigen/Dense>
 
+#include "mg400_common/kinematics.hpp"
+
 namespace mg400_common
 {
-const double J1_MIN = -160 * M_PI / 180.0;
-const double J1_MAX = 160 * M_PI / 180.0;
-const double J2_MIN = -25 * M_PI / 180.0;
-const double J2_MIN_NO_COLLISION = -19 * M_PI / 180.0;
-const double J2_MAX = 85 * M_PI / 180.0;
-const double J3_MIN = -25 * M_PI / 180.0;
-const double J3_MAX = 105 * M_PI / 180.0;
-const double J31_MIN = -60 * M_PI / 180.0;
-const double J31_MAX = 60 * M_PI / 180.0;
-const double J4_MIN = -180 * M_PI / 180.0;
-const double J4_MAX = 180 * M_PI / 180.0;
+const double J1_MIN = kinematics::J1_MIN;
+const double J1_MAX = kinematics::J1_MAX;
+const double J2_MIN = kinematics::J2_MIN;
+const double J2_MIN_NO_COLLISION = kinematics::J2_MIN_NO_COLLISION;
+const double J2_MAX = kinematics::J2_MAX;
+const double J3_MIN = kinematics::J3_MIN;
+const double J3_MAX = kinematics::J3_MAX;
+const double J31_MIN = kinematics::J3_1_MIN;
+const double J31_MAX = kinematics::J3_1_MAX;
+const double J4_MIN = kinematics::J4_MIN;
+const double J4_MAX = kinematics::J4_MAX;
 
-const double LINK1_X = 0.0435;
-const double LINK1_Y = 0.0;
-const double LINK1_Z = 0.0;
+const double LINK1_X = kinematics::LINK1(0);
+const double LINK1_Y = kinematics::LINK1(1);
+const double LINK1_Z = kinematics::LINK1(2);
 
-const double LINK2_X = 0.0;
-const double LINK2_Y = 0.0;
-const double LINK2_Z = 0.175;
+const double LINK2_X = kinematics::LINK2(0);
+const double LINK2_Y = kinematics::LINK2(1);
+const double LINK2_Z = kinematics::LINK2(2);
 
-const double LINK3_X = 0.175;
-const double LINK3_Y = 0.0;
-const double LINK3_Z = 0.0;
+const double LINK3_X = kinematics::LINK3(0);
+const double LINK3_Y = kinematics::LINK3(1);
+const double LINK3_Z = kinematics::LINK3(2);
 
-const double LINK4_X = 0.066;
-const double LINK4_Y = 0.0;
-const double LINK4_Z = -0.0538;
+const double LINK4_X = kinematics::LINK4(0);
+const double LINK4_Y = kinematics::LINK4(1);
+const double LINK4_Z = kinematics::LINK4(2);
 
 class MG400IKUtil
 {
 private:
-  const double MARGIN = 1 * M_PI / 180.0;
-  const double ROUND_DECIMALS = 8;
-
-  const Eigen::Vector3d LINK1;
-  const Eigen::Vector3d LINK2;
-  const Eigen::Vector3d LINK3;
-  const Eigen::Vector3d LINK4;
+  static constexpr double ROUND_DECIMALS = 8;
 
 public:
-  MG400IKUtil();
+  MG400IKUtil() = default;
   bool InMG400Range(const std::vector<double> &);
   std::vector<double> InverseKinematics(const std::vector<double> &);
   std::vector<double> ToolCoordToBaseCoord(
--- mg400_common/src/mg400_ik_util.cpp
+++ mg400_common/src/mg400_ik_util.cpp
@@ -14,84 +14,86 @@
 
 #include "mg400_common/mg400_ik_util.hpp"
 
-namespace mg400_common
+#include <cmath>
+#include <stdexcept>
+
+namespace
 {
-MG400IKUtil::MG400IKUtil()
-: LINK1({LINK1_X, LINK1_Y, LINK1_Z}),
-  LINK2({LINK2_X, LINK2_Y, LINK2_Z}),
-  LINK3({LINK3_X, LINK3_Y, LINK3_Z}),
-  LINK4({LINK4_X, LINK4_Y, LINK4_Z})
+
+constexpr size_t KINEMATICS_POSE_SIZE = 4;
+
+void validate_vector_size(const std::vector<double> & values, size_t expected_minimum_size)
 {
+  if (values.size() < expected_minimum_size) {
+    throw std::invalid_argument("MG400IKUtil input must contain at least 4 elements.");
+  }
 }
 
+Eigen::Vector4d to_eigen4(const std::vector<double> & values)
+{
+  validate_vector_size(values, KINEMATICS_POSE_SIZE);
+
+  Eigen::Vector4d result;
+  result << values[0], values[1], values[2], values[3];
+  return result;
+}
+
+std::vector<double> to_legacy_joint_vector(
+  const Eigen::Vector4d & joints,
+  double round_decimals)
+{
+  std::vector<double> rounded_angles = {
+    joints(0), joints(1), joints(2), joints(3), 0.0, 0.0
+  };
+
+  const double round_scale = std::pow(10.0, round_decimals);
+  for (auto & angle : rounded_angles) {
+    angle = std::round(angle * round_scale) / round_scale;
+  }
+
+  return rounded_angles;
+}
+
+}  // namespace
+
+namespace mg400_common
+{
 bool MG400IKUtil::InMG400Range(const std::vector<double> & angles)
 {
-  double j1 = angles[0];
-  double j2 = angles[1];
-  double j3 = angles[2];
-  double j4 = angles[3];
-
-  bool is_j1_within_limit = (J1_MIN + MARGIN <= j1 && j1 <= J1_MAX - MARGIN);
-  bool is_j2_within_limit =
-    (j3 > 0) ? (J2_MIN + MARGIN <= j2 &&
-    j2 <= J2_MAX - MARGIN) : (J2_MIN_NO_COLLISION + MARGIN <= j2 && j2 <= J2_MAX - MARGIN);
-  bool is_j3_within_limit = (J3_MIN + MARGIN <= j3 && j3 <= J3_MAX - MARGIN);
-  bool is_j31_within_limit = (J31_MIN + MARGIN <= (j3 - j2) && (j3 - j2) <= J31_MAX - MARGIN);
-  bool is_j4_within_limit = (J4_MIN + MARGIN <= j4 && j4 <= J4_MAX - MARGIN);
-
-  return is_j1_within_limit && is_j2_within_limit &&
-         is_j3_within_limit && is_j31_within_limit && is_j4_within_limit;
+  const Eigen::Vector4d joints = to_eigen4(angles);
+  return kinematics::check_constraints(joints).allValid();
 }
 
 std::vector<double> MG400IKUtil::InverseKinematics(
   const std::vector<double> & tool_vec)
 {
-  double p_x = tool_vec[0];
-  double p_y = tool_vec[1];
-  double p_z = tool_vec[2];
-  double Rx = tool_vec[3];
-
-  double pp_x = sqrt(pow(p_x, 2) + pow(p_y, 2)) - this->LINK4[0] - this->LINK1[0];
-  double pp_z = p_z - this->LINK4[2] - this->LINK1[2];
-
-  double length2 = this->LINK2.norm();
-  double length3 = this->LINK3.norm();
-
-  double j_1 = atan2(p_y, p_x);
-
-  double val1 =
-    (pow(pp_x, 2) + pow(pp_z, 2) - pow(length2, 2) - pow(length3, 2)) / (2 * length2 * length3);
-  if (val1 < -1 || val1 > 1) {
+  const Eigen::Vector4d pose = to_eigen4(tool_vec);
+  Eigen::Vector4d angles;
+  try {
+    angles = kinematics::ik_for_flange_from_origin(pose);
+  } catch (const std::runtime_error &) {
     throw std::runtime_error("Inverse kinematics error");
   }
 
-  double j_3_1 = acos(val1);
-  double j_2 = M_PI / 2 - atan2(pp_z, pp_x) - asin(
-    length3 / sqrt(pow(pp_x, 2) + pow(pp_z, 2)) * sin(j_3_1));
-
-  double j_3 = j_2 + j_3_1 - M_PI / 2;
-  double j_4 = Rx - j_1;
-
-  std::vector<double> angles = {j_1, j_2, j_3, j_4, 0.0, 0.0};
-  for (auto & angle : angles) {
-    angle = round(angle * pow(10, ROUND_DECIMALS)) / pow(10, ROUND_DECIMALS);
-  }
-  if (!this->InMG400Range(angles)) {
+  if (!kinematics::check_constraints(angles).allValid()) {
     throw std::runtime_error("Outside of workspace.");
   }
 
-  return angles;
+  return to_legacy_joint_vector(angles, ROUND_DECIMALS);
 }
 
 std::vector<double> MG400IKUtil::ToolCoordToBaseCoord(
   const std::vector<double> & vec,
   const std::vector<double> & tool_coord)
 {
+  validate_vector_size(vec, KINEMATICS_POSE_SIZE);
+  validate_vector_size(tool_coord, KINEMATICS_POSE_SIZE);
+
   std::vector<double> pos(3);
   for (int i = 0; i < 3; ++i) {
     pos[i] = vec[i] - tool_coord[i];
   }
-  double ang = vec[3] + tool_coord.back();
+  double ang = vec[3] + tool_coord[3];
 
   return std::vector<double>{pos[0], pos[1], pos[2], ang, 0.0, 0.0};
 }
